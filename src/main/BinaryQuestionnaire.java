fe921227f8f931d7baf37315d7aa231046320030
michaelfn <michaelfognielsen@hotmail.com>
2023-02-01 11:54:03 +0100

    Added k-means clustering to BinaryQuestionnaire

--- src/main/BinaryQuestionnaire.java
+++ src/main/BinaryQuestionnaire.java
@@ -1,11 +1,16 @@
 package main;
 
 import java.io.File;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 
 public class BinaryQuestionnaire implements Dataset {
+    public static final boolean[] NIP_SCORE_LOOKUP = {
+            false,false,false,true,true,false,true,false,true,true,
+            false,false,false,false,true,false,true,true,true,true,
+            false,true,true,false,false,true,false,true,false,false,
+            false,true,false,false,true,false,false,false,false,true
+    };
+
     private boolean[][] answers;
 
     public int getNumberOfParticipants() {
@@ -116,4 +121,80 @@ public class BinaryQuestionnaire implements Dataset {
         }
         return size;
     }
+
+    //Performs the K-means clustering on a binary data set
+    public int[] kMeans(final boolean[] scoreLookupArray) {
+        int K = 2;  //Amount of clusters
+        int[] scores = getQuestionnaireScores(scoreLookupArray);    //Score of each participant used to calculate distance from centroid
+        int[] resultingClusters = new int[getNumberOfParticipants()];   //The resulting cluster each participant is assigned to
+
+        //Place centroids randomly
+        Random r = new Random();
+        int[] centroids = new int[K];
+        int[] tempCentroids = new int[K];
+        for (int i = 0; i < K; i++) {
+            int n = r.nextInt(getNumberOfQuestions());
+            centroids[i] = n;
+            tempCentroids[i] = n;
+        }
+        while (true) {
+            for (int i = 0; i < getNumberOfParticipants(); i++) {
+
+                //Find nearest centroid
+                int min = Integer.MAX_VALUE;
+                int cluster = -1;
+                for (int k = 0; k < K; k++) {
+                    int dist = Math.abs(scores[i] - centroids[k]);
+                    if (dist < min) {
+                        min = dist;
+                        cluster = k;
+                    }
+                }
+                //Assign participant to cluster
+                resultingClusters[i] = cluster;
+            }
+
+            //Update centroid means
+            for (int k = 0; k < K; k++) {
+                int sum = 0;
+                int count = 0;
+                for (int i = 0; i < getNumberOfParticipants(); i++) {
+                    if (resultingClusters[i] == k) {
+                        sum += scores[i];
+                        count++;
+                    }
+                }
+                centroids[k] = sum / count;
+            }
+
+            //Break loop if centroids haven't moved; else update temporary centroids
+            boolean b = true;
+            for (int k = 0; k < K; k++) {
+                if (tempCentroids[k] != centroids[k]) {
+                    b = false;
+                    tempCentroids[k] = centroids[k];
+                }
+            }
+
+            if (b) {
+                break;
+            }
+        }
+        return resultingClusters;
+    }
+
+    private int[] getQuestionnaireScores(final boolean[] scoreLookupArray) {
+        int n = getNumberOfParticipants();
+        int[] scores = new int[n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < getNumberOfQuestions(); j++) {
+                if (answers[i][j] == scoreLookupArray[j]) {
+                    scores[i]++;
+                }
+            }
+        }
+        return scores;
+    }
+
+
 }
--- src/main/Main.java
+++ src/main/Main.java
@@ -5,7 +5,14 @@ import java.util.Date;
 public class Main {
     public static void main(String[] args) {
         BinaryQuestionnaire questionnaire = new BinaryQuestionnaire();
-        questionnaire.loadAnswersFromFile("NPI.csv", 1, -1, 1, 40);
-        TangleClusterer.generateClusters(questionnaire, 1500);
+        questionnaire.loadAnswersFromFile("NPI.csv", 1, 1000, 1, 40);
+        TangleClusterer.generateClusters(questionnaire, 150);
+
+        System.out.println("\nkMeans:");
+        int[] kMeansResult = questionnaire.kMeans(BinaryQuestionnaire.NIP_SCORE_LOOKUP);
+        System.out.println("Resulting clustering for first 50 participants: ");
+        for (int i = 0; i < 50; i++) {
+            System.out.print(kMeansResult[i] + " ");
+        }
     }
 }
