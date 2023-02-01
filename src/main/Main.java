147958374e1054dc5e57d25dd3751ca83475da48
JensLaustrup <s204431@student.dtu.dk>
2023-02-01 16:54:50 +0100

    Added time print for k-means

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,5 +1,6 @@
 package main;
 
+import java.util.Date;
 import java.util.Random;
 
 public class Main {
@@ -7,11 +8,15 @@ public class Main {
         BinaryQuestionnaire questionnaire = new BinaryQuestionnaire();
         questionnaire.loadAnswersFromFile("NPI.csv", 1, -1, 1, 40);
         TangleClusterer.generateClusters(questionnaire, 1500);
+        long time1 = new Date().getTime();
         System.out.println("\nkMeans:");
         int[] kMeansResult = questionnaire.kMeans(BinaryQuestionnaire.NIP_SCORE_LOOKUP);
         System.out.println("Resulting clustering for first 50 participants: ");
         for (int i = 0; i < 50; i++) {
             System.out.print(kMeansResult[i] + " ");
         }
+        long time2 = new Date().getTime();
+        System.out.println();
+        System.out.println("K-means time: " + (time2-time1) + " ms");
     }
 }
