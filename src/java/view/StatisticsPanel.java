b38e3e17b165e58d13fda5acc02166412925a381
michaelfn <michaelfognielsen@hotmail.com>
2023-03-02 12:13:50 +0100

    Drew image in statistics panel

--- src/java/view/StatisticsPanel.java
+++ src/java/view/StatisticsPanel.java
@@ -1,10 +1,12 @@
 package view;
 
 import datasets.Dataset;
+import datasets.FeatureBasedDataset;
 import smile.plot.swing.ScatterPlot;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.image.BufferedImage;
 
 public class StatisticsPanel extends JPanel {
     private View view;
@@ -14,9 +16,6 @@ public class StatisticsPanel extends JPanel {
 
         setPreferredSize(new Dimension(view.getWindowWidth(), view.getWindowHeight()));
         setLayout(null);
-
-
-
     }
 
     @Override
@@ -25,6 +24,18 @@ public class StatisticsPanel extends JPanel {
 
         g.setColor(Color.BLACK);
         g.fillRect(10, 10, 100, 100);
+
+        BufferedImage image = null;
+
+        Dataset dataset = view.getDataset();
+        if (dataset instanceof FeatureBasedDataset) {
+            ScatterPlot plot = ScatterPlot.of(((FeatureBasedDataset) dataset).dataPoints);
+            image = plot.canvas().toBufferedImage(view.windowWidth, view.windowHeight - view.topPanelHeight);
+        }
+
+        if (image != null) {
+            g.drawImage(image, 0, 0, this);
+        }
     }
 
     protected void setBounds() {
